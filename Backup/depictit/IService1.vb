4956b3aadd39697b0bd202c6853f7f658144d954
OzzyMaul2 <tim.hill@gmx.de>
2015-03-11 11:43:17 +0100

    nicht funktionale Änderungen zur interoperabiliät zu VS 2012

---
+++ Backup/depictit/IService1.vb
@@ -0,0 +1,30 @@
+﻿' HINWEIS: Mit dem Befehl "Umbenennen" im Kontextmenü können Sie den Schnittstellennamen "IService1" sowohl im Code als auch in der Konfigurationsdatei ändern.
+<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function GetData(ByVal value As String) As String
+
+    <OperationContract()>
+    Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType
+
+    ' TODO: Hier Dienstvorgänge hinzufügen
+
+    <OperationContract()>
+    Function GetPicture(ByVal Source As String) As Object
+
+
+End Interface
+
+' Verwenden Sie einen Datenvertrag, wie im folgenden Beispiel dargestellt, um Dienstvorgängen zusammengesetzte Typen hinzuzufügen.
+
+<DataContract()>
+Public Class CompositeType
+
+    <DataMember()>
+    Public Property BoolValue() As Boolean
+
+    <DataMember()>
+    Public Property StringValue() As String
+
+End Class
